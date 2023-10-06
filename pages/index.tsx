a828e6ffe5253c3cbbacc0bc5e64829b27deb44a
Christian Dinata <christiandinata@MacBook-Air.local>
2023-10-06 22:23:00 +0700

    changed pages folder structure

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,10 @@
 import type { NextPage } from 'next';
 import {useSession, signIn, signOut} from 'next-auth/react'
 import Image from 'next/image';
-import { Header, HeaderContainer, Left, StyledLoggedInMenu, LoginButton, Menu, Text, MenuProps, DropdownMenu, DropdownHeader, AvatarLabelGroup, LabelGroup, LabelName, LabelEmail, DropdownItems, DropdownItem, Divider, NavMenu, XIcon, DropdownMenu2, NotLoggedInContainer, LoginButton2 } from './header.styles';
-import { About, AboutDesc, AboutEmail, Heading, AboutInfo, AboutText, AboutVisitorCount, Content, Desc, SectionTitle, Profile, ProfileDesc, Repos, Username, Visitors, AvatarGroup, VisitorSection, RepoHeader, Badge, RepoCards, InnerDesc } from './content.styles';
+import { Header, HeaderContainer, Left, StyledLoggedInMenu, LoginButton, Menu, Text, MenuProps, DropdownMenu, DropdownHeader, AvatarLabelGroup, LabelGroup, LabelName, LabelEmail, DropdownItems, DropdownItem, Divider, NavMenu, XIcon, DropdownMenu2, NotLoggedInContainer, LoginButton2 } from '../styles/header.styles';
+import { About, AboutDesc, AboutEmail, Heading, AboutInfo, AboutText, AboutVisitorCount, Content, Desc, SectionTitle, Profile, ProfileDesc, Repos, Username, Visitors, AvatarGroup, VisitorSection, RepoHeader, Badge, RepoCards, InnerDesc } from '../styles/content.styles';
 import RepoCard from '@/components/RepoCard';
-import { Footer, FooterContainer, FooterContent } from './footer.styles';
+import { Footer, FooterContainer, FooterContent } from '../styles/footer.styles';
 import {  useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
 
